48ae00a9bd337be69b2531492ec47d0d986a170c
aranryan <aran.ryan@gmail.com>
2019-11-29 19:39:20 -0500

    add palette

--- R/colors.R
+++ R/colors.R
@@ -1,38 +1,76 @@
-oe_colors <- c(
-  `prime_blue`   = rgb(0,52,105, max=255),
-  `prime_grey`   = rgb(123, 124, 119, max=255),
-  `prime_ltgrey` = rgb(241, 239, 237, max=255),
-  `prime_white`  = rgb(255, 255, 255, max=255),
-  `sec_grey`     = rgb(232, 236, 244, max=255),
-  `sec_must`     = rgb(209, 162, 30, max=255),
-  `sec_aqua`     = rgb(110, 145, 160, max=255),
-  `sec_blue`     = rgb(67, 110, 172, max=255),
-  `sec_brblue`   = rgb(0, 173, 220, max=255),
-  `blue_grad_1`  = rgb(0, 52, 105, max=255),
-  `blue_grad_2`  = rgb(0, 70, 126, max=255),
-  `blue_grad_3`  = rgb(39, 90, 150, max=255),
-  `blue_grad_4`  = rgb(58, 112, 174, max=255),
-  `blue_grad_5`  = rgb(0, 131, 187, max=255),
-  `blue_grad_6`  = rgb(0, 150, 202, max=255),
-  `blue_grad_7`  = rgb(0, 173, 220, max=255),
-  `blue_grad_8`  = rgb(0, 183, 225, max=255),
-  `blue_grad_9`  = rgb(30, 194, 229, max=255),
-  `blue_grad_10` = rgb(113, 207, 235, max=255),
-  `bold_blue`    = rgb(0, 52, 105, max=255),
-  `bold_ltblue`  = rgb(0, 173, 220, max=255),
-  `bold_grey`    = rgb(123, 124, 119, max=255),
-  `bold_red`     = rgb(189, 27, 33, max=255),
-  `bold_mustard` = rgb(209, 162, 30, max=255),
-  `bold_green`   = rgb(0, 121, 63, max=255),
-  `bold_orange`  = rgb(222, 99, 40, max=255),
-  `bold_purple`  = rgb(150, 87, 147, max=255),
-  `pale_blue`    = rgb(0, 173, 220, max=255),
-  `pale_midblue` = rgb(75, 199, 231, max=255),
-  `pale_ltblue`  = rgb(173, 224, 242, max=255),
-  `pale_grey`    = rgb(123, 124, 119, max=255),
-  `pale_midgrey` = rgb(169, 166, 162, max=255),
-  `pale_ltgrey`  = rgb(208, 205, 201, max=255),
-  `pale_mustard` = rgb(197, 137, 48, max=255),
-  `pale_dkbeige` = rgb(218, 177, 119, max=255),
-  `pale_beige`   = rgb(233, 212, 180, max=255)
-)
+
+#' An Oxford Economics palette generator
+#'
+#' Color palettes based on the OE style guide
+#'
+#' @param name Name of desired palette. Choices are:
+#'   \code{primary}, \code{secondary},  \code{blue_grad},
+#'   \code{bold}, \code{pale}
+#' @param n Number of colors desired.
+#' @param type Either "continuous" or "discrete". Use continuous if you want
+#'   to automatically interpolate between colours.
+#'
+#' @return A vector of colours.
+#' @export
+#'
+#' @examples
+#' #' # If you need more colours than normally found in a palette, you
+#' # can use a continuous palette to interpolate between existing
+#' # colours
+#' pal <- wes_palette(21, name = "Zissou1", type = "continuous")
+#' image(volcano, col = pal)
+#'
+#' we can't directly feed the palette into scale_fill_manual because
+#' the colors are named in the vector. If one provides scale_fill_manual
+#' a named vector, then ggplot2 is trying to match the colors to the
+#' values in the dataframe
+#'
+#'
+#'
+#'
+#'
+
+
+oe_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- oe_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
+}
+
+
+#' Title
+#'
+#' @param x
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+}
